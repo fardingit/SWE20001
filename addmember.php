fe9cc65aa16d853d26b6115303680e9b46958163
Rafia S. Chowdhury <104883615+Rafia22@users.noreply.github.com>
2023-09-22 23:23:36 +1000

    Update addmember.php

--- addmember.php
+++ addmember.php
@@ -2,23 +2,30 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="author" content="Rafia"/>
+    <link rel="stylesheet" href="style.css">
     <title>Add Member</title>
-    <link rel="stylesheet" href="styles/style.css">
 </head>
 <body>
     <form id="addMemberForm">
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name" required>
+        <label for="firstName">First Name:</label>
+        <input type="text" id="firstName" name="firstName" required><br>
+
+        <label for="lastName">Last Name:</label>
+        <input type="text" id="lastName" name="lastName" required><br>
         
+        <label for="contact">Contact:</label>
+        <input type="tel" id="contact" name="contact" pattern="(?:\04\)?[1-9]\d\d\)?\d{3}\d{3}" required>
+        <small>Format: (04xx) xxx xxx</small><br>
+
         <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required>
-        
-        <!-- Add any additional fields as needed -->
-        
-        <input type="submit" value="Add Member">
+        <input type="email" id="email" name="email" required><br>
+
+        <label for="address">Address:</label>
+        <input type="text" id="address" name="address" required><br>
+
+        <input type="submit" value="Confirm">
     </form>
-    <script src="formHandler.js"></script>
+
+    <script src="script.js"></script>
 </body>
 </html>

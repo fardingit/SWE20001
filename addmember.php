fa2813a6695b8e96b7d660b6c0041dabe20c9407
01YM <137867978+01YM@users.noreply.github.com>
2023-10-03 17:42:11 +1100

    members/ grocery validation
    Adding validation to members & grocery

--- addmember.php
+++ addmember.php
@@ -8,17 +8,17 @@
 <body>
     <form id="addMemberForm">
         <label for="firstName">First Name:</label>
-        <input type="text" id="firstName" name="firstName" required><br>
+        <input type="text" id="firstName" name="firstName" pattern="/^[a-zA-Z' ]{0,20}+$/" required><br>
 
         <label for="lastName">Last Name:</label>
-        <input type="text" id="lastName" name="lastName" required><br>
+        <input type="text" id="lastName" name="lastName" pattern="/^[a-zA-Z' ]{0,20}+$/" required><br>
         
         <label for="contact">Contact:</label>
         <input type="tel" id="contact" name="contact" pattern="(?:\04\)?[1-9]\d\d\)?\d{3}\d{3}" required>
         <small>Format: (04xx) xxx xxx</small><br>
 
         <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required><br>
+        <input type="email" id="email" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required><br>
 
         <label for="address">Address:</label>
         <input type="text" id="address" name="address" required><br>
